e64b263b870f6b43dfdee0cf78b46962fdb6488c
aln <alnfgit@gmail.com>
2016-11-13 21:36:54 +0200

    pdata change, script cleanup

--- scripts/raws_preprocessing.R
+++ scripts/raws_preprocessing.R
@@ -7,18 +7,19 @@ source("pcaPlots.R")
 
 # General
 studies <- read.table("../pdata/studies.tsv", header = TRUE, sep = "\t")
+exclude = TRUE
 
-# London
-i=1
+# London=1, Oslo=2
+i=2
 path = paste("../raws/", studies[i,]$ID, sep="")
 pdata = read.table(paste("../pdata/", studies[i,]$ID, "_pdata.tsv", sep=""), sep="\t", head=TRUE, stringsAsFactors = FALSE)
 idatfiles = dir(path, pattern="idat", full.name=TRUE)
-pdata <- pdata[c(-23, -24),]
-idatfiles <- idatfiles[c(-23, -24)]
 
-susp = which(pdata$Comment=="Suspicious")
-pdata <- pdata[-susp,]
-idatfiles <- idatfiles[-susp]
+if (exclude) {
+  exl = which((pdata$Comment=="Suspicious") | (pdata$Comment=="Exclude"))
+  pdata <- pdata[-exl,]
+  idatfiles <- idatfiles[-exl]
+}
 
 raw.data <- readIdatFiles(idatfiles)
 raw.data@phenoData = AnnotatedDataFrame(pdata)
@@ -29,19 +30,24 @@ batch = as.factor(pData(N.data)$Sentrix_ID)
 mod = model.matrix(~as.factor(Condition), data=pData(N.data))
 #mod = model.matrix(~1, data=pData(N.data))
 combat_edata = ComBat(dat=exprs(N.data), batch=batch, mod=mod, par.prior=TRUE, prior.plots=FALSE)
-N.data@assayData$exprs <- combat_edata
+N.data.nobatch <- N.data
+N.data.nobatch@assayData$exprs <- combat_edata
 
 # Perform PCA and plot
 pca = prcomp(t(exprs(N.data)))
+pca.nobatch = prcomp(t(exprs(N.data.nobatch)))
 pl <- pcaPlots(pca, pData(N.data), c("Condition", "Sentrix_ID"), studies[i,]$ID)
+pl.nobatch <- pcaPlots(pca.nobatch, pData(N.data.nobatch), c("Condition", "Sentrix_ID"), studies[i,]$ID)
 
 # Save plot for manual quality control
 save_plot(paste("../plots/", studies[i,]$ID, "_PCA.pdf", sep=""),
-          base_height=4, base_aspect_ratio = 2.2, pl)
+          base_height=4, base_aspect_ratio = 2.6, pl)
+save_plot(paste("../plots/", studies[i,]$ID, "_PCA_nobatch.pdf", sep=""),
+          base_height=4, base_aspect_ratio = 2.6, pl.nobatch)
 
 # AQM
-arrayQualityMetrics(expressionset = N.data,
-                    outdir = "../plots/Report_1",
-                    force = TRUE,
-                    do.logtransform = FALSE,
-                    intgroup = c("Condition"))
+# arrayQualityMetrics(expressionset = N.data,
+#                     outdir = "../plots/Report",
+#                     force = TRUE,
+#                     do.logtransform = FALSE,
+#                     intgroup = c("Condition"))
